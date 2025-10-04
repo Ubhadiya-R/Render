a6489fc869ac77ba93e1af2124118f539bb54766
Riya Ubhadiya <riyaubhadiya43@gmail.com>
2025-10-04 13:02:05 +0530

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+
+
+
+app.get('/', (req, res) => {
+
+    res.send("welcome to mca");
+
+});
+
+
+
+
+app.listen(3000, () => {
+
+    console.log("server is running : 127.0.0.1:3000");
+
+});
